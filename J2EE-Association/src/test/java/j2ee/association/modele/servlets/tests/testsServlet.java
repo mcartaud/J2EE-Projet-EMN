b8f96a05430b6cca1f46bef2c1cd28dea0c9179c
mcartaud <mathieu.cartaud@etudiant-mines.nantes.fr>
2013-09-27 08:27:55 +0200

    add .jsp files for eeach views. add maven dependencies for servlets and annotations

---
+++ J2EE-Association/src/test/java/j2ee/association/modele/servlets/tests/testsServlet.java
@@ -0,0 +1,14 @@
+package j2ee.association.modele.servlets.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testsServlet {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
